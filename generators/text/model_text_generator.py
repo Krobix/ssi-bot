0b462714ed679f2aedfa16b79ce5736045442a8a
Brendon Shaw <brendon.shaw8921@gmail.com>
2025-02-04 12:15:13 -0500

    i need to commit because i forgot to earlier but i forgot what actual changes i made.

--- generators/text/model_text_generator.py
+++ generators/text/model_text_generator.py
@@ -148,7 +148,9 @@ class ModelTextGenerator(threading.Thread, TaggingMixin):
 
 		if self.llama is not None:
 			logging.info("Generating text using llama")
-			gen = self.llama(prompt=prompt, temperature=float(self.temperature), max_tokens=512)["choices"][0]["text"]
+			gen = self.llama(prompt=prompt, temperature=float(self.temperature), max_tokens=512)["choices"][0]["text"].split("<|")[0]
+			gen += "<|"
+			gen=prompt+gen
 			logging.info(f"llama finished generating: {str(gen)}")
 			#llama is too fucking fast apparently?
 			time.sleep(300)
